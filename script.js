b708a9b3ebfa0224adf476cca0cb83585f68b835
Fardan11 <fardankabir11@gmail.com>
2024-01-21 21:32:11 +0600

    'End'

--- script.js
+++ script.js
@@ -12,3 +12,36 @@ const createBooking = function (flightNum, NumPassenger = 1, price = 199) {
 };
 
 createBooking('LHI123');
+createBooking('LHI123', 2, 800);
+createBooking('LHI123', 2);
+createBooking('LHI123', undefined, 5); // if we dont set any value then we can do undefined
+
+const flight = 'LM1234';
+const fardan = {
+  name: 'Fardan Kabir Chowdhury',
+  passport: 213242424,
+};
+
+const checkIn = function (flightNum, Passenger) {
+  //flightNum has the copy of flight
+  flightNum = 'LH999';
+  Passenger.name = 'Mr.' + Passenger.name;
+  if (Passenger.passport === 213242424) {
+    alert('check In');
+  } else {
+    alert('Wrong Passport');
+  }
+};
+
+checkIn(flight, fardan);
+console.log(flight);
+console.log(fardan);
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000);
+};
+
+newPassport(fardan);
+checkIn(flight, fardan);
+
+//Js doesnt have passing by reference, just have Passing by Values
